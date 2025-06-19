cc3a5d1ee054dc1ce6e27f497172057c6aea72e2
R S Chiraag <chiraag.rs@gmail.com>
2025-06-19 11:04:45 +0530

    add frontend url

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,10 @@ const port = 3000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            "https://dontchat-cyl18.vercel.app/"
+        ],
         methods: ["GET", "POST"]
     }
 });
