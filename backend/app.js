ac6e2359e83ac7e9368d247c8052f1fe541ef8dd
R S Chiraag <chiraag.rs@gmail.com>
2025-06-19 11:32:04 +0530

    fix port

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
